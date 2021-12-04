a107e7f0c4532b91c0fb3db81d010eda766ebb3d
Ragul1902 <ragulsenthilkumar02@gmail.com>
2021-12-04 11:48:49 +0530

    Added RSA encryption and decryption code

--- Cipher_Algorithms.cpp
+++ Cipher_Algorithms.cpp
@@ -1,22 +1,50 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-const string menu = "Enter 1 to rotate every character in the string\nEnter 2 to encode it to base64\nEnter 3 to decode it from base64\nEnter 4 to end program\n-->";
+const string menu = "Enter 1 to print menu\n"
+                    "Enter 2 to rotate every character in the string\n"
+                    "Enter 3 to encode it to base64\n"
+                    "Enter 4 to decode it from base64\n"
+                    "Enter 5 to RSA encrypt a number\n"
+                    "Enter 6 to RSA decrypt a number\n"
+                    "Enter 7 to keysquare encrypt a number\n"
+                    "Enter 8 to keysquare decrypt a number\n"
+                    "Enter 9 to end program\n"
+                    "-->";
+
 const string charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-void rotateCharacters(string s, int val){
-    
-    val %= 26;
-    for(auto &i : s){
-        if(i >= 'A' and i <= 'Z')
-        i =  (i - 'A' + val)%26 + 'A';
+
+long long binpow(long long a, long long b, long long m)
+{
+    a %= m;
+    long long res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
     }
-    cout<<s<<endl;
+    return res;
+}
+
+void rotateCharacters(string s, int val)
+{
+
+    val %= 26;
+    for (auto &i : s)
+    {
+        if (i >= 'A' and i <= 'Z')
+            i = (i - 'A' + val) % 26 + 'A';
+    }
+    cout << s << endl;
     return;
 }
 
-void encodeToBase64(string str){
+void encodeToBase64(string str)
+{
     string result = "";
     int i;
     int j;
@@ -28,30 +56,34 @@ void encodeToBase64(string str){
     int value = 0;
     int count = 0;
 
-    for(i = 0; i < len; i += 3){
+    for (i = 0; i < len; i += 3)
+    {
         value = 0;
         count = 0;
         numberOfBits = 0;
-        
-        for(j = i; j < len && j <= i+2; j++){
+
+        for (j = i; j < len && j <= i + 2; j++)
+        {
             value = value << 8;
             value = value | str[j];
             count += 1;
-    
         }
 
         numberOfBits = 8 * count;
-        padding = numberOfBits%3;
+        padding = numberOfBits % 3;
 
-        while(numberOfBits != 0){
+        while (numberOfBits != 0)
+        {
 
-            if(numberOfBits >= 6){
+            if (numberOfBits >= 6)
+            {
 
                 temp = numberOfBits - 6;
-                index = (value >> temp)  & 63;
+                index = (value >> temp) & 63;
                 numberOfBits -= 6;
             }
-            else {
+            else
+            {
                 temp = 6 - numberOfBits;
                 index = (value << temp) & 63;
                 numberOfBits = 0;
@@ -61,105 +93,152 @@ void encodeToBase64(string str){
         }
     }
 
-    for(i = 1; i <= padding; i++){
+    for (i = 1; i <= padding; i++)
+    {
         result.push_back('=');
     }
 
-    cout<<result<<endl;
+    cout << result << endl;
     return;
 }
 
-int findIndexInCharSet(char c){
-    for(int i = 0; i < 64; i++){
-        if(c == charSet[i]){
+int findIndexInCharSet(char c)
+{
+    for (int i = 0; i < 64; i++)
+    {
+        if (c == charSet[i])
+        {
             return i;
         }
     }
     return -1;
-} 
+}
 
-void decodeFromBase64(string str){
+void decodeFromBase64(string str)
+{
     string result = "";
     int i;
     int j;
     int value = 0;
     int numberOfBits = 0;
     int len = str.length();
-    for(i = 0; i < len; i += 4 ){
+    for (i = 0; i < len; i += 4)
+    {
         value = 0;
         numberOfBits = 0;
-        for(j = i; j < len && j <= i+3 ;j++){
-            if(str[j] != '='){
+        for (j = i; j < len && j <= i + 3; j++)
+        {
+            if (str[j] != '=')
+            {
                 value = value << 6;
                 numberOfBits += 6;
             }
 
             int idx = findIndexInCharSet(str[j]);
-            if(idx != -1){
+            if (idx != -1)
+            {
                 value = value | idx;
             }
-            else{
+            else
+            {
                 value = value >> 2;
                 numberOfBits -= 2;
             }
         }
-        while(numberOfBits != 0){
+        while (numberOfBits != 0)
+        {
             numberOfBits -= 8;
             result.push_back((value >> numberOfBits) & 255);
         }
     }
 
-    cout<<result<<endl;
+    cout << result << endl;
     return;
 }
 
-
-
-
-
-int main(){
-    while(1){
+int main()
+{
+    while (1)
+    {
         string s;
         int val;
-        cout<<menu;
+        cout << menu;
         int option;
-        cin>>option;
+        cin >> option;
         switch (option)
         {
         case 1:
-            cout<<"Enter the input string: ";
-            cin>>s;
-            cout<<"Enter the number of character places to rotate:";
-            cin>>val;
-            cout<<"\nOutput string: "<<endl;
-            rotateCharacters(s,val);
-            cout<<endl;            
+            cout<<menu;
             break;
-        
+
         case 2:
-            cout<<"Enter the input string: ";
-            cin>>s;
-            cout<<"\nOutput string: "<<endl;
-            encodeToBase64(s);
-            cout<<endl;
+            cout << "Enter the input string: ";
+            cin >> s;
+            cout << "Enter the number of character places to rotate:";
+            cin >> val;
+            cout << "\nOutput string: " << endl;
+            rotateCharacters(s, val);
+            cout << endl;
             break;
 
         case 3:
-            cout<<"Enter the input string: ";
-            cin>>s;
-            cout<<"\nOutput string: "<<endl;
-            decodeFromBase64(s);
-            cout<<endl;
+            cout << "Enter the input string: ";
+            cin >> s;
+            cout << "\nOutput string: " << endl;
+            encodeToBase64(s);
+            cout << endl;
             break;
-        
+
         case 4:
+            cout << "Enter the input string: ";
+            cin >> s;
+            cout << "\nOutput string: " << endl;
+            decodeFromBase64(s);
+            cout << endl;
+            break;
+
+        case 5:
+        {
+            cout << "Enter the value to be encrypted: ";
+            long long num;
+            cin >> num;
+            cout << "Enter value of e: ";
+            long long e;
+            cin >> e;
+            cout << "Enter value of N: ";
+            long long N;
+            cin >> N;
+            long long ans = binpow(num, e, N);
+            cout << "Cipher is : " << ans << endl<<endl;
+            break;
+        }
+        case 6:
+        {
+            cout << "Enter the value to be decrypted: ";
+            long long num;
+            cin >> num;
+            cout << "Enter value of d: ";
+            long long d;
+            cin >> d;
+            cout << "Enter value of N: ";
+            long long N;
+            cin >> N;
+            long long ans = binpow(num, d, N);
+            cout << "Cipher is : " << ans << endl<<endl;
+            break;
+        }
+
+        case 7:
+        
+        case 8:
+
+        case 9:
             return 0;
 
         default:
-            cout<<"Enter a valid option"<<endl;
+            cout << "Enter a valid option" << endl;
             break;
         }
     }
     return 0;
 }
-
